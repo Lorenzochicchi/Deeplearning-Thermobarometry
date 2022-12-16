2632fd3312e15387bc03988a1e88a635e2c80ee7
Lorenzo Chicchi <38913744+Lorenzochicchi@users.noreply.github.com>
2022-12-16 16:20:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ if st.button('Starting prediction'):
       label="Download data as xlsx",
       data=df_output,
       file_name= 'Prediction'+nametuple[0]+'.xlsx',
+      mime='text/csv'
   )
   st.download_button(
       label="Download data as csv",
