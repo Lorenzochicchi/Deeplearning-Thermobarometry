4972e471b6f8369ca509bc47e93e7cb115478b68
Lorenzo Chicchi <38913744+Lorenzochicchi@users.noreply.github.com>
2022-12-19 10:43:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,31 @@ def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file2(png_file) 
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-attachment: scroll; # doesn't work
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+  
+  
+  
+  
+set_background('./imgs/background.png')
     
 im = Image.open("D4V.ico")
 st.set_page_config(
