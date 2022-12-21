d01162d006d59d21635ac09388b84eaa7aabb426
Lorenzo Chicchi <38913744+Lorenzochicchi@users.noreply.github.com>
2022-12-21 15:53:59 +0100

    Update info.py

--- pages/info.py
+++ pages/info.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as ps
 
 st.title("Info")
+st.header("References")
+
+st.header("Input structure")
+st.write("The input dataset must be a .csv o .xlsx file with the following two structure:")
+st.write("**Only clinopyroxene datase")
+df = pd.read_excel('example_input.xlsx')
+st.table(df)
